761b4d24eba278bfe7832ae2e67bc3056ba293aa
quentinsorin <quentin.sorin2018@campus-eni.fr>
2020-03-18 15:37:55 +0100

    Initial commit

---
+++ TP_Chat_Module5/Controllers/ChatController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP_Chat_Module5.Models;
+
+namespace TP_Chat_Module5.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<Chat> chats;
+        public List<Chat> Chats => chats ?? (chats = Chat.GetMeuteDeChats());
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(Chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            var chat = Chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var chat = Chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var chat = Chats.FirstOrDefault(c => c.Id == id);
+                if (chat != null)
+                {
+                    Chats.Remove(chat);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TP_Chat_Module5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP_Chat_Module5.MvcApplication" Language="C#" %>
